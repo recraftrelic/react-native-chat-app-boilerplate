551d49e37f351b296b5be5df62975e79b3ddf4d8
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-02-12 23:16:42 +0530

    added searchbar

---
+++ components/UI/SearchBar.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { View, TextInput, StyleSheet, ViewStyle } from "react-native";
+import useTheme from "../../hooks/useTheme";
+import { AppTheme } from "../../config/DefaultConfig";
+
+interface Props {
+  placeHolder: string
+}
+
+const SearchBar: React.FunctionComponent<Props> = ({
+  placeHolder
+}: Props) => {
+  const theme: AppTheme = useTheme();
+
+  return (
+    <View style={[style.searchContainer, { borderBottomColor: theme.lightBottomColor }]}>
+      <View style={style.textContainer}>
+        <TextInput
+          placeholder={placeHolder}
+          placeholderTextColor={theme.lightTextColor}
+          style={{ color: theme.textColor }}
+        />
+      </View>
+      <View style={style.iconContainer}>
+        <Icon name="search" size={15} color={theme.lightTextColor} />
+      </View>
+    </View>
+  )
+}
+
+export default SearchBar;
+
+interface Style {
+  searchContainer: ViewStyle;
+  iconContainer: ViewStyle;
+  textContainer: ViewStyle;
+}
+
+const style: Style = StyleSheet.create<Style>({
+  textContainer: {
+    flex: 9
+  },
+  searchContainer: {
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 10,
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: "flex-end"
+  }
+});
