eac99c9060ae092c7688f50492e21d015bf65543
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-03-12 16:38:17 +0530

    image-loader

--- components/Chat/ChatProfileMedia.tsx
+++ components/Chat/ChatProfileMedia.tsx
@@ -1,40 +1,56 @@
-import React from 'react';
-import { ScrollView, StyleSheet, View, Image, ImageStyle, ViewStyle, ImageSourcePropType } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, View, Image, ImageStyle, ViewStyle, ImageSourcePropType, ActivityIndicator } from 'react-native';
+import { AppTheme } from '../../config/DefaultConfig';
+import useTheme from '../../hooks/useTheme';
 
 interface Props{
     source: ImageSourcePropType;
-};
+}
 
 const ChatProfileMedia: React.FunctionComponent<Props> = ({
     source
 }: Props) => {
 
-    return (
-        <View style={style.container}>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <Image
-            style={style.image}
-            source={source}
-          />
-          <Image
-            style={style.image}
-            source={source}
-          />
-          <Image
-            style={style.image}
-            source={source}
-          />
-          <Image
-            style={style.image}
-            source={source}
-          />
-          <Image
-            style={style.image}
-            source={source}
-          />
-        </ScrollView>
-        </View>
-    );
+  const theme: AppTheme = useTheme();
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  const gallery = [
+    {
+      source: source
+    },
+    {
+      source: source
+    },
+    {
+      source: source
+    },
+    {
+      source: source
+    },
+    {
+      source: source
+    }
+  ];
+
+  return (
+    <View style={style.container}>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {loaded ? null : (
+          <ActivityIndicator animating={true} size="large" color={theme.appColor} style={style.imageStyle}/>
+        )}
+        {
+          gallery.map((data,index) => 
+            <Image
+              style={style.image}
+              source={data.source}
+              onLoad={() => setLoaded(true)}
+              key={index}
+            />
+          )
+        }
+      </ScrollView>
+    </View>
+  );
 };
 
 export default ChatProfileMedia;          
@@ -42,6 +58,7 @@ export default ChatProfileMedia;
 interface Style {
     image: ImageStyle
     container: ViewStyle;
+    imageStyle: ImageStyle;
 }
 
 const style: Style = StyleSheet.create<Style>({
@@ -52,5 +69,10 @@ const style: Style = StyleSheet.create<Style>({
     container: {
         flexDirection: 'row',
         padding: 20,
+    },
+    imageStyle: {
+      position: "absolute",
+      top: 65,
+      left: 75,
     },
 })
