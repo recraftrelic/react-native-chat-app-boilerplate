2767b690e09f35a72dbc77334d4c9c9658a36c9a
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-03-12 11:25:04 +0530

    image-loader

--- components/Chat/ChatUserImage.tsx
+++ components/Chat/ChatUserImage.tsx
@@ -14,6 +14,7 @@ const { height, width } = Dimensions.get('window')
 const ChatUserImage: React.FunctionComponent<Props> = ({ source, containerStyle, imageStyle }: Props) => {
   const theme: AppTheme = useTheme();
   const [isModalsVisible, setModalVisible] = useState<boolean>(false);
+  const [loaded, setLoaded] = useState<boolean>(false);
 
     return (
       <View style={[containerStyle]}>
@@ -25,21 +26,27 @@ const ChatUserImage: React.FunctionComponent<Props> = ({ source, containerStyle,
           onRequestClose={() => {alert('Modal');}}>
           <TouchableOpacity style={[style.modalContainer, {backgroundColor: theme.modalBackgroundColor}]} activeOpacity={1.0} onPress={() => {setModalVisible(false);}}>
             <View>
+              {loaded ? null : (
+                <ActivityIndicator animating={true} size="large" color={theme.appColor} style={style.modalImageStyle}/>
+              )}
               <Image
                 style={[imageStyle, {width: 250, height: 250, borderRadius: 0}]}
                 source={source}
+                onLoad={() => setLoaded(true)}
               />
-              <ActivityIndicator animating={false} size="small" color={theme.appColor} style={style.imageStyle}/>
             </View>
           </TouchableOpacity>
         </Modal> 
         : null }
         <TouchableOpacity onPress={() => {setModalVisible(true);}}>
+          {loaded ? null : (
+            <ActivityIndicator animating={true} size="small" color={theme.appColor} style={style.imageStyle}/>
+          )}
           <Image
             style={imageStyle}
             source={source}
+            onLoad={() => setLoaded(true)}
           />
-          <ActivityIndicator animating={true} size="small" color={theme.appColor} style={style.imageStyle}/>
         </TouchableOpacity>
       </View>
     )
@@ -49,16 +56,22 @@ export default ChatUserImage;
 
 interface Style {
   modalContainer: ViewStyle;
+  modalImageStyle: ImageStyle;
   imageStyle: ImageStyle;
 }
 
 const style: Style = StyleSheet.create<Style>({
   modalContainer: {
-      flexDirection: 'column',
-      justifyContent: "center",
-      alignItems: 'center',
-      width: width,
-      height: height,
+    flexDirection: 'column',
+    justifyContent: "center",
+    alignItems: 'center',
+    width: width,
+    height: height,
+  },
+  modalImageStyle: {
+    position: "absolute",
+    top: 105,
+    left: 105,
   },
   imageStyle: {
     position: "absolute",
