c8bfc8f4662eb84720cb0f48e93156a65c785f1d
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-03-23 12:55:16 +0530

    interlization

--- pages/ChatSetting/index.tsx
+++ pages/ChatSetting/index.tsx
@@ -7,13 +7,14 @@ import ChatProfileInfo from '../../components/Chat/ChatProfileInfo';
 import ThemeToggle from '../../components/Chat/ThemeToggle';
 import ChatVersion from '../../components/Chat/ChatVersion';
 import { ThemeKey } from '../../config/themes';
-import { TouchableOpacity, View, ViewStyle, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, ViewStyle, StyleSheet, TextStyle } from 'react-native';
 import ThemedText from '../../components/UI/ThemedText';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { AppTheme, AppConstants } from '../../config/DefaultConfig';
 import useTheme from '../../hooks/useTheme';
 import useConstants from '../../hooks/useConstants';
 import { settingConstant } from './constants.';
+import ModalDropdown from 'react-native-modal-dropdown';
 
 interface Props extends RouteComponentProps {
     dispatch: Dispatch
@@ -37,6 +38,8 @@ const ChatSetting: React.FunctionComponent<Props> = ({
 
     const {userName, userInfo, appVersion} = settingConstant;
 
+    const languages = ['English','French','Spanish','German','Chinese'];
+
     return (
         <>
         <ChatProfileInfo
@@ -46,6 +49,14 @@ const ChatSetting: React.FunctionComponent<Props> = ({
           onButtonPress={goBack}
           editInfo={true}
         />
+        <View style={[style.container, {borderWidth: 0}]}>
+            <View style={style.leftContainer}>
+                <ThemedText styleKey="textColor">Default Language</ThemedText>
+            </View>
+            <View style={style.rightContainer}>
+                <ModalDropdown options={languages} defaultValue = 'English' animated={true} textStyle={style.listStyle} dropdownStyle={style.downStyle} dropdownTextStyle={style.languageStyle}/>
+            </View>
+        </View>
         <ThemeToggle updateTheme={updateTheme} />
         <TouchableOpacity onPress={goToLogin}>
             <View style={[style.container, {borderColor: theme.lightBottomColor}]}>
@@ -68,21 +79,24 @@ interface Style {
     container: ViewStyle;
     leftContainer: ViewStyle;
     rightContainer: ViewStyle;
+    languageStyle: TextStyle;
+    listStyle: TextStyle;
+    downStyle: ViewStyle;
   }
   
-  const style: Style = StyleSheet.create<Style>({
-      container: {
-          flexDirection: 'row',
-          justifyContent: "space-between",
-          alignItems: 'center',
-          paddingLeft: 20,
-          paddingRight: 20,
-          marginTop: 10,
-          marginLeft: 20,
-          marginRight: 20,
-          paddingTop: 10,
-          paddingBottom: 10,
-          borderWidth: 2,
+const style: Style = StyleSheet.create<Style>({
+    container: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignItems: 'center',
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginTop: 10,
+        marginLeft: 20,
+        marginRight: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderWidth: 2,
       },  
     leftContainer: {
       alignItems: "flex-start",
@@ -90,4 +104,13 @@ interface Style {
     rightContainer: {
       alignItems: "flex-end",
     },
-  });
+    languageStyle: {
+      fontSize: 16,
+    },
+    listStyle: {
+      fontSize: 14,
+    },
+    downStyle: {
+        width: 150,
+    }
+});
