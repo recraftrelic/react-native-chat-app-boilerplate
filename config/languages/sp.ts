f0a3ea9538383b1192bddd7f65412d1d73483f18
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-03-24 16:10:35 +0530

    language-refactor

---
+++ config/languages/sp.ts
@@ -0,0 +1,35 @@
+import { AppLanguage } from '../languages'
+
+export const sp: AppLanguage = {
+  searchPlacerHolder: "Chats de búsqueda",
+  messagePlacerHolder: "Escriba Texto aquí para el mensaje ...",
+  namePlaceholder: "Nombre completo",
+  usernamePlacerHolder: "Nombre de usuario",
+  emailPlacerHolder: "Email",
+  passwordPlacerHolder: "Contraseña",
+  loginButton: "Iniciar sesión",
+  logoutButton: "Cerrar sesión",
+  signupButton: "Regístrate",
+  signupCheck: "Si no tienes una cuenta",
+  chatTitle: "Chats",
+  appName: "Recraft Chat App",
+  blockButton: "Bloquear",
+  reportButton: "Reporte",
+  deleteButton: "Eliminar la conversación",
+  defaultLanguage: "Idioma predeterminado",
+  defaultTheme: "Tema oscuro",
+  loginValidation : {
+    username: `Se requiere nombre de usuario`,
+    password: `se requiere contraseña`,
+    passwordLength: 'Longitud de contraseña entre 6 y 12',
+  },
+  signupValidation : {
+    name: `Nombre completo es requerido`,
+    username: `Se requiere nombre de usuario`,
+    email: `Correo electronico es requerido`,
+    validEmail: 'Por favor introduzca una dirección de correo electrónico válida',
+    password: `se requiere contraseña`,
+    passwordLength: 'Longitud de contraseña entre 6 y 12',
+  },
+}
+  
